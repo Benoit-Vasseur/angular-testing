3882f5604b0c352baef655c0f9c7c2cdefc48ade
Benoit Vasseur <benoit.vasseur@zenika.com>
2023-06-06 22:59:52 +0200

    delete some tests

--- src/app/model/hero.service.spec.ts
+++ src/app/model/hero.service.spec.ts
@@ -40,20 +40,7 @@ describe ('HeroesService (with spies)', () => {
   });
 
   it('should return an error when the server returns a 404', (done: DoneFn) => {
-    const errorResponse = new HttpErrorResponse({
-      error: 'test 404 error',
-      status: 404, statusText: 'Not Found'
-    });
-
-    httpClientSpy.get.and.returnValue(asyncError(errorResponse));
-
-    heroService.getHeroes().subscribe({
-      next: heroes => done.fail('expected an error, not heroes'),
-      error: error  => {
-        expect(error.message).toContain('test 404 error');
-        done();
-      }
-    });
+    // TODO
   });
 
 });
