093f9cbd42899da53609a617ac84a7cfc53de278
Benoit Vasseur <benoit.vasseur@zenika.com>
2023-06-06 22:53:09 +0200

    add cypress cpt testing

---
+++ src/app/stepper/stepper.component.cy.ts
@@ -0,0 +1,24 @@
+import { StepperComponent } from './stepper.component'
+
+describe('StepperComponent', () => {
+    it('supports an "initial" prop to set the value', () => {
+        cy.mount(StepperComponent, {
+            componentProperties: {
+                count: 100,
+            },
+        })
+        cy.get('[data-cy=counter]').should('have.text', '100')
+    })
+
+    it('when the increment button is pressed, the counter is incremented', () => {
+        cy.mount(StepperComponent)
+        cy.get('[data-cy=increment]').click()
+        cy.get('[data-cy=counter]').should('have.text', '1')
+    })
+
+    it('when the decrement button is pressed, the counter is decremented', () => {
+        cy.mount(StepperComponent)
+        cy.get('[data-cy=decrement]').click()
+        cy.get('[data-cy=counter]').should('have.text', '-1')
+    })
+})
